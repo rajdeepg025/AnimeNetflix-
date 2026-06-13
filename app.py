9894dafd25db93150d290307ee0e5b16d5346920
rajdeepg025 <rajdeepg783@gmail.com>
2026-06-13 22:03:28 +0530

    search system added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 import os
 
 app = Flask(__name__)
 
+# Railway MySQL connection using environment variables
 # Railway MySQL connection using environment variables
 db = mysql.connector.connect(
     host="roundhouse.proxy.rlwy.net",
@@ -71,29 +72,65 @@ def reading():
 def search():
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM anime where category_id=2")
-    category_result_2 = cursor.fetchall()
+    query = request.args.get("q", "")
 
-    cursor.execute("SELECT * FROM book")
-    book_result = cursor.fetchall()
+    anime_results = []
+    book_results = []
+    music_results = []
+
+    if query:
+
+        cursor.execute(
+            "SELECT * FROM anime WHERE title LIKE %s",
+            (f"%{query}%",)
+        )
+        anime_results = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT * FROM book WHERE title LIKE %s",
+            (f"%{query}%",)
+        )
+        book_results = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT * FROM music WHERE title LIKE %s",
+            (f"%{query}%",)
+        )
+        music_results = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM book WHERE category_id = 8")
+    Manhwa_result = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM book WHERE category_id = 7")
+    Manga_result = cursor.fetchall()
 
     cursor.execute("SELECT * FROM music")
     music_result = cursor.fetchall()
 
-    cursor.execute("SELECT * FROM anime where category_id=1")
-    category_result_1 = cursor.fetchall()
+    cursor.execute("SELECT * FROM anime WHERE category_id = 2")
+    category_result_2 = cursor.fetchall()
 
-    cursor.execute("SELECT * FROM anime where category_id=4")
+    cursor.execute("SELECT * FROM anime WHERE category_id = 4")
     category_result_4 = cursor.fetchall()
 
     return render_template(
         "search.html",
-        book_list=book_result,
+
+        anime_results=anime_results,
+        book_results=book_results,
+        music_results=music_results,
+
+        category_2_list=category_result_2,
+        category_4_list=category_result_4,
+
         music_list=music_result,
-        category_1_list = category_result_1,
-        category_4_list = category_result_4,
-        category_2_list = category_result_2
+        Manhwa_list=Manhwa_result,
+        Manga_list=Manga_result,
+
+        query=query
+        
     )
+
 
 
 if __name__ == "__main__":
